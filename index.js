a8d600e9e631607d04047869b4ab05fc6b131640
ivemobileapp6 <124156205+ivemobileapp6@users.noreply.github.com>
2023-04-30 11:13:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const Route = require("./route/route");
+// const UserRoute = require("./route/user");
+const mongoose = require('mongoose');
+const app = express();
+const fetch = require("node-fetch")
+
+mongoose.set('strictQuery', true)
+mongoose.connect(
+  "mongodb+srv://ivemobileapp6:Fra4rf5tg@cluster0.tisbkgu.mongodb.net/?retryWrites=true&w=majority").then(() => app.listen(10888)).then(() => console.log("Connected To Database")).catch((err) => console.log(err));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/', Route)
+
+const mySecret = process.env['TOKEN_SECRET']
